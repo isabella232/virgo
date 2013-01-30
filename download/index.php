cc932c96ed8b71eeadb21959e74cb214b20457f5
Glyn Normington <gnormington@vmware.com>
2013-01-30 10:13:27 +0000

    advertise survey

--- download/index.php
+++ download/index.php
@@ -35,7 +35,7 @@
 <div id="midcolumn">
 	<table cellspacing="0" border="1">
 			<tr>
-				<td align="center"><p style="font-size:20px" style="color:#191970>
+				<td align="center"><p style="font-size:20px" style="color:#191970">
 					We'd really appreciate it if you could fill in the<br />
 				 <a href="https://docs.google.com/spreadsheet/viewform?formkey=dC1JMXRCbzd4a2pVN285ZU5fRlJoM2c6MQ">Virgo community survey</a>
 				 <br />before the deadline of 14 February 2013.</p>
