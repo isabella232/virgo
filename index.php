bd063044343d70c925a6e5138b22c0fa4b6fcf19
GianMaria Romanato <gian.maria.romanato@finantix.com>
2016-09-14 10:08:07 +0200

    alphabetical sorting of virgo powered list

--- index.php
+++ index.php
@@ -157,10 +157,10 @@
 		<div class="sideitem">
 			<h6>Virgo Powered</h6>
 			<ul>
+				<li><a href="http://ariane.echinopsii.net/">Ariane by Echinopsii</li>
 			    <li><a href="http://blurryme.com/">BlurryMe App</a></li>
 			    <li><a href="http://www.eclipse.org/virgo/references/cmegroup.php">CME Group</li>
-			    <li><a href="http://bigblog.tportal.hr/btin#29857">Croatian Telecom</li>
-			    <li><a href="http://ariane.echinopsii.net/">Ariane by Echinopsii</li>
+			    <li><a href="http://bigblog.tportal.hr/btin#29857">Croatian Telecom</li>			    
 			    <li><a href="http://underlap.blogspot.com/2011/10/how-infor-ion-uses-virgo.html">Infor ION</a></li>
 			    <li><a href="http://www.konekti.org/">Konekti Platform</a></li>
 			    <li><a href="http://www.infoq.com/news/2012/12/sap-netweaver-cloud">SAP NetWeaver Cloud</a></li>
