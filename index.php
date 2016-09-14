4f44631277d06652d198e465af54443d6439eddf
GianMaria Romanato <gian.maria.romanato@finantix.com>
2016-09-14 09:42:40 +0200

    Add Ariane to Virgo Powered list

--- index.php
+++ index.php
@@ -160,6 +160,7 @@
 			    <li><a href="http://blurryme.com/">BlurryMe App</a></li>
 			    <li><a href="http://www.eclipse.org/virgo/references/cmegroup.php">CME Group</li>
 			    <li><a href="http://bigblog.tportal.hr/btin#29857">Croatian Telecom</li>
+			    <li><a href="http://ariane.echinopsii.net/">Ariane by Echinopsii</li>
 			    <li><a href="http://underlap.blogspot.com/2011/10/how-infor-ion-uses-virgo.html">Infor ION</a></li>
 			    <li><a href="http://www.konekti.org/">Konekti Platform</a></li>
 			    <li><a href="http://www.infoq.com/news/2012/12/sap-netweaver-cloud">SAP NetWeaver Cloud</a></li>
