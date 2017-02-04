58e9f2fdb16e919e9e3543ae7f7e636fc3a5f3a9
GianMaria Romanato <gm.romanato@gmail.com>
2017-02-04 17:33:13 +0100

    Add Intelliment Security to Virgo powered

--- index.php
+++ index.php
@@ -164,6 +164,7 @@
 			    <li><a href="http://www.geppetto.org">Geppetto - scientific visualization and simulation platform</a></li>
 			    <li><a href="http://underlap.blogspot.com/2011/10/how-infor-ion-uses-virgo.html">Infor ION</a></li>
 			    <li><a href="http://www.konekti.org/">Konekti Platform</a></li>
+			    <li><a href="http://www.intellimentsec.com">Policy Automation by Intelliment Security</a></li>
 			    <li><a href="http://www.infoq.com/news/2012/12/sap-netweaver-cloud">SAP NetWeaver Cloud</a></li>
 				<li><a href="http://github.com/PouzinSociety/tinos">TINOS research project</a></li>
 				<li><a href="http://underlap.blogspot.co.uk/2012/10/virgo-in-vsphere.html">VMware vSphere</a></li>
