18784ad3f8a69652b9437bcb8f55e028f6e35cb9
Florian Waibel <fwaibel@eclipsesource.com>
2017-02-08 21:01:42 +0100

    Update milestone docs for user- and programmer guide

--- documentation/index.php
+++ documentation/index.php
@@ -1,12 +1,12 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
-	$App 	= new App();	
-	$Nav	= new Nav();	
-	$Menu 	= new Menu();		
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+	$App 	= new App();
+	$Nav	= new Nav();
+	$Menu 	= new Menu();
 	include($App->getProjectCommon());
-	
+
 /*******************************************************************************
  * Copyright (c) 2009, 2015 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
@@ -15,37 +15,37 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors: Christopher Frost
- *    
+ *
  *******************************************************************************/
 
-	# Begin: page-specific settings.  Change these. 
+	# Begin: page-specific settings.  Change these.
 	$pageTitle 		= "Virgo OSGi Runtime - Documentation";
 	$pageKeywords	= "Eclipse, EclipseRT, Virgo, OSGi, Documentation";
 	$pageAuthor		= "Christopher Frost";
-	
+
 	$currentVersion = "3.6.4.RELEASE";
 	$currentBundlorVersion = "1.1.2.RELEASE";
 	$milestone = "3.7.0.M01";
+	$virgoMilestone = "3.7.0.RC01";
 	$toolingMilestone = "3.5.0.M04";
 	ob_start();
 ?>
-	
+
 	<div id="midcolumn">
 		<h2>Documentation</h2>
 		<p>
 			The documentation for Virgo can be downloaded from <a href="http://www.eclipse.org/virgo/download/" target="_self">here</a> or it is browseable online below:
-			
+
 			<ul>
 				<li>User Guide ( <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-user-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-user-guide/htmlsingle/virgo-user-guide.html">HTML Single</a> )</li>
-				
-				<li>Programmer Guide ( <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-programmer-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-programmer-guide/htmlsingle/virgo-programmer-guide.html">HTML Single</a> )</li>
-				
-				<li>Snaps Guide ( <a href="virgo-documentation-<?=$currentVersion ?>/docs/snaps-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$currentVersion ?>/docs/snaps-guide/htmlsingle/virgo-snaps-guide.html">HTML Single</a> )</li>
-			
-			    <li>Tooling Guide ( <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-tooling-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-tooling-guide/htmlsingle/virgo-tooling-guide.html">HTML Single</a> )</li>
 
-			    <li>Bundlor User Guide ( <a href="bundlor-documentation-<?=$currentBundlorVersion ?>/docs/user-guide/html/index.html">HTML</a> | <a href="bundlor-documentation-<?=$currentBundlorVersion ?>/docs/user-guide/htmlsingle/user-guide.html">HTML Single</a> )</li>
-			
+				<li>Programmer Guide ( <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-programmer-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-programmer-guide/htmlsingle/virgo-programmer-guide.html">HTML Single</a> )</li>
+
+				<li>Snaps Guide ( <a href="virgo-documentation-<?=$currentVersion ?>/docs/snaps-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$currentVersion ?>/docs/snaps-guide/htmlsingle/virgo-snaps-guide.html">HTML Single</a> )</li>
+
+				<li>Tooling Guide ( <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-tooling-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$currentVersion ?>/docs/virgo-tooling-guide/htmlsingle/virgo-tooling-guide.html">HTML Single</a> )</li>
+
+				<li>Bundlor User Guide ( <a href="bundlor-documentation-<?=$currentBundlorVersion ?>/docs/user-guide/html/index.html">HTML</a> | <a href="bundlor-documentation-<?=$currentBundlorVersion ?>/docs/user-guide/htmlsingle/user-guide.html">HTML Single</a> )</li>
 			</ul>
 		</p>
 		<p>
@@ -55,18 +55,18 @@
 		    Users of earlier releases of Virgo, of SpringSource dm Server, or of the "slices" framework should refer to the <a href="http://wiki.eclipse.org/Virgo/Community#Migration_Notes">migration notes</a> on the wiki.
 		</p>
 
-		
+
  		<h2>Milestone Documentation</h2>
 		<p>
 			The documentation for the latest milestone of Virgo can be downloaded from <a href="http://www.eclipse.org/virgo/download/" target="_self">here</a> or it is browseable online below:
-			
+
 			<ul>
-				<li>User Guide ( <a href="virgo-documentation-<?=$milestone ?>/docs/virgo-user-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$milestone ?>/docs/virgo-user-guide/htmlsingle/virgo-user-guide.html">HTML Single</a> )</li>
-				
-				<li>Programmer Guide ( <a href="virgo-documentation-<?=$milestone ?>/docs/virgo-programmer-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$milestone ?>/docs/virgo-programmer-guide/htmlsingle/virgo-programmer-guide.html">HTML Single</a> )</li>
-				
+				<li>User Guide ( <a href="virgo-documentation-<?=$virgoMilestone ?>/docs/virgo-user-guide/html5/virgo-user-guide.html">HTML</a> )</li>
+
+				<li>Programmer Guide ( <a href="virgo-documentation-<?=$virgoMilestone ?>/docs/virgo-programmer-guide/html5/virgo-programmer-guide.html">HTML</a> )</li>
+
 				<li>Snaps Guide ( <a href="virgo-documentation-<?=$milestone ?>/docs/snaps-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$milestone ?>/docs/snaps-guide/htmlsingle/virgo-snaps-guide.html">HTML Single</a> )</li>
-				
+
 				<li>Tooling Guide ( <a href="virgo-documentation-<?=$milestone ?>/docs/virgo-tooling-guide/html/index.html">HTML</a> | <a href="virgo-documentation-<?=$milestone ?>/docs/virgo-tooling-guide/htmlsingle/virgo-tooling-guide.html">HTML Single</a> )</li>
 
 			</ul>
