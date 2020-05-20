5c0297015f7676d1b4634d3502c30be90616e4ca
Florian Waibel <fwaibel@datenkollektiv.de>
2020-05-20 10:55:44 +0200

    Fix download link to Virgo nano full

--- download/index.php
+++ download/index.php
@@ -44,7 +44,7 @@
                 <li><strong>Virgo Jetty Server</strong> -  <a href='http://www.eclipse.org/downloads/download.php?file=/virgo/release/VP/3.7.3.RELEASE/virgo-jetty-server-3.7.3.RELEASE.zip' target='_self'>Download</a> </li>
                 <li><strong>Virgo Kernel</strong> - <a href='http://www.eclipse.org/downloads/download.php?file=/virgo/release/VP/3.7.3.RELEASE/virgo-kernel-3.7.3.RELEASE.zip' target='_self'>Download</a></li>
                 <li><strong>Virgo Nano</strong> - <a href='http://www.eclipse.org/downloads/download.php?file=/virgo/release/VP/3.7.3.RELEASE/virgo-nano-3.7.3.RELEASE.zip' target='_self'>Download</a></li>
-                <li><strong>Virgo Nano Full</strong> - <a href='http://www.eclipse.org/downloads/download.php?file=/virgo/release/VP/3.7.3.RELEASE/virgo-nano-3.7.3.RELEASE.zip' target='_self'>Download</a></li>
+                <li><strong>Virgo Nano Full</strong> - <a href='http://www.eclipse.org/downloads/download.php?file=/virgo/release/VP/3.7.3.RELEASE/virgo-nano-full-3.7.3.RELEASE.zip' target='_self'>Download</a></li>
                 <!-- <li><strong>Virgo Snaps</strong> - <a href='http://www.eclipse.org/downloads/download.php?file=/virgo/release/VP/3.7.2.RELEASE/snaps-3.7.2.RELEASE.zip' target='_self'>Download</a></li> -->
                 <!-- <li><strong>Virgo Update Site</strong> - <a href='http://download.eclipse.org/virgo/release/updatesite/3.7.2.RELEASE' target='_self'>http://download.eclipse.org/virgo/release/updatesite/3.7.2.RELEASE</a></li> -->
             </ul>
